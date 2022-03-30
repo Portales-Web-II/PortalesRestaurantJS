f79b4a574cb9ca98f30865732ca7a2dc87af95be
HectorOC-e <osoriohector89@gmail.com>
2022-03-30 16:02:02 -0600

    Commit Error

--- src/routes/rutaProducto.js
+++ src/routes/rutaProducto.js
@@ -6,7 +6,7 @@ const router = Router();
 const pagina = "Menu"
 
 router.get('/', (req, res) => {
-    res.render('products' , {pagina});
+    res.render('menu' , {pagina});
 });
 
 module.exports = router;

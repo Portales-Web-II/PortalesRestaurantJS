03160e318ec09d8c01fcfc2eff790ec569515833
Elvia <elviatany968@gmail.com>
2022-04-05 22:31:57 -0600

    carrito

---
+++ src/config/db.js
@@ -0,0 +1,24 @@
+const sequelize = require('sequelize');
+/*const db = new sequelize(
+    'dbportalesrestaurant', //nombre de la base de datos
+    'portales', //usuario de la base de datos
+    'Portales123@', //contraseña de la base de datos
+
+    {
+        host: '3.93.152.196',//servidor
+        dialect: 'mysql',
+        port: '3306',
+    }
+);*/
+const db = new sequelize(
+    'dbportalesrestaurant', //nombre de la base de datos
+    'root', //usuario de la base de datos
+    '', //contraseña de la base de datos
+
+    {
+        host: 'localhost',//servidor
+        dialect: 'mysql',
+        port: '3306',
+    }
+);
+module.exports = db;
---
+++ src/models/modeloFacturacion.js
@@ -0,0 +1,45 @@
+const sequelize = require('sequelize');
+const db = require('../config/db');
+
+//Definimos los campos de las tablas
+const Facturacion = db.define(
+    "facturacion",
+    {
+        idFactura:{
+            type: sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+
+        isv:{
+            type: sequelize.DOUBLE,
+            default: "0.5",
+            allowNull: false,
+        },
+
+        total:{
+            type: sequelize.DOUBLE,
+            allowNull: false,
+        },
+
+        fecha:{
+            type: sequelize.DATE,
+            allowNull: true,
+        },
+
+        idPedido:{
+            type: sequelize.INTEGER,
+            allowNull: false,
+        },
+       
+    },
+
+    {
+        tableName: "facturacion",
+        timestamps: false,
+    }
+);
+
+//Exportamos este modelo
+module.exports = Facturacion;

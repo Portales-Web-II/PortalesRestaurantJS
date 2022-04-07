dec6c73109b5ed01a7462e6639a97db68c8d9df7
SamuelOviedo <isaaccoviedo@gmail.com>
2022-04-07 13:50:07 -0600

    Validaciones facturación

--- src/public/js/validaciones.js
+++ src/public/js/validaciones.js
@@ -14,3 +14,31 @@ function isNumericKey(evt) {
         return true;
     return false;
 }
+
+function validateModalFactura() {
+
+    var nom = document.forms["facturaForm"]["nombreCli"].value;
+    var apell = document.forms["facturaForm"]["apellidoCli"].value;
+    var tel = document.forms["facturaForm"]["telefonoCli"].value;
+    var dir = document.forms["facturaForm"]["direccionCli"].value;
+
+    var cam = document.forms["facturaForm"]["cambio"].value;
+
+    var rV = document.getElementById('radioVisa');
+    var rM = document.getElementById('radioMc');
+    var numT = document.forms["facturaForm"]["numeroTar"].value;
+    var cvc = document.forms["facturaForm"]["cvc"].value;
+    var ven = document.forms["facturaForm"]["vencimiento"].value;
+
+    if (nom != "" && apell != "" && tel != "" && dir != "" && cam != "") {
+        $('#exampleModal').modal('show');
+    }
+    else if (rV.checked == true || rM.checked == true && numT != "" && cvc != "" && ven != ""){
+        $('#exampleModal').modal('show');
+    }
+    else{
+        $('#ErrorModal').modal('show');
+    }
+    // $('#exampleModal').modal('toggle');
+    //$('#exampleModal').modal('hide');
+}
